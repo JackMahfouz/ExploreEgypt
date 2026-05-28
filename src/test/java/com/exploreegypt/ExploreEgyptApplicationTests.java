4fabc114eb64888bce27b7dc837357051719d347
Jack Isaac <jackmahfouz766@gmail.com>
2026-05-28 14:48:02 +0300

    Explore Egypt Intiation

---
+++ src/test/java/com/exploreegypt/ExploreEgyptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploreegypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreEgyptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
