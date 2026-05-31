837989bf445c543cb1856ea56e224f3bf00f3c06
Jack Isaac <jackmahfouz766@gmail.com>
2026-05-31 16:58:26 +0300

    create data access layer for service for basic crud ops

---
+++ src/main/java/com/exploreegypt/repository/CafeRepository.java
@@ -0,0 +1,11 @@
+package com.exploreegypt.repository;
+
+import com.exploreegypt.entity.Cafe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CafeRepository extends JpaRepository<Cafe, UUID> {
+}
---
+++ src/main/java/com/exploreegypt/repository/HotelRepository.java
@@ -0,0 +1,11 @@
+package com.exploreegypt.repository;
+
+import com.exploreegypt.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, UUID> {
+}
